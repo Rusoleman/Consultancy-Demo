796f0ffe25fa6d95a9eff9dd21784023f902891b
Rusoleman <example@mail.com>
2023-04-13 00:03:11 -0600

    Added animations and JS code

--- scripts/main.js
+++ scripts/main.js
@@ -2,8 +2,7 @@ console.log("Welcome to my site!");
 
 //FASE 4 (*Elemento temporal*)
 let  tag = document.getElementById("tag"); 
-let count = 0;
-const counter = () => {
-    count++;
-    tag.innerHTML = count;
+const message = () => {
+    tag.innerHTML = "Welcome!";
 }
+
